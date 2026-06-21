e5831168f1bf1d3c773d9e4b7301467967d2c30a
nryadav18 <nedurirajeswaryadav5@gmail.com>
2026-06-21 15:55:10 +0530

    backend update - 3

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,6 @@ app.use('/api/upload', uploadRoutes);
 app.use('/api/attendance', attendanceRoutes);
 app.use('/api/profile', profileRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
